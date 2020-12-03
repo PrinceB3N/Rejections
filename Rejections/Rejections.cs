a6dfea22e80b932a49b7ffe5a797ead2b43d91cf
PrinceB3N <benjaminprince@ucsb.edu>
2020-12-03 04:47:57 -0800

    test

--- Rejections/Rejections.cs
+++ Rejections/Rejections.cs
@@ -13,23 +13,23 @@ public class Rejects : Rejections
     double wait_time;
     public List<string> IgnoreList { get; set; }
     Dictionary<string, Tuple<DateTime, int>> reject_symbols;
-/// <param name="wait_time">the minimum time between requests (in minutes).</param>
-/// <param name="max_rejections">max # of times a request for a symbol can be rejected before it can no longer be permitted. </param>
+    /// <param name="wait_time">the minimum time between requests (in minutes).</param>
+    /// <param name="max_rejections">max # of times a request for a symbol can be rejected before it can no longer be permitted. </param>
     public Rejects(double wait_time, int max_rejections)
     {
         this.wait_time = wait_time;
         this.max_rejections = max_rejections;
         this.reject_symbols = new Dictionary<string, Tuple<DateTime, int>>();
     }
-/// <param name="symbol">the name of the stock.</param>
-/// <returns>Tuple of the last rejected request's time, and # of rejected requests for the symbol. </returns>
+    /// <param name="symbol">the name of the stock.</param>
+    /// <returns>Tuple of the last rejected request's time, and # of rejected requests for the symbol. </returns>
     public Tuple<DateTime, int> getRecord(string symbol){
         if(symbol==null){return null;}
         if (this.reject_symbols.ContainsKey(symbol)) { return this.reject_symbols[symbol]; }
         else { return null; }
     }
-/// <param name="symbol">the name of the stock.</param>
-/// <param name="now">the time of the rejected request. </param>
+    /// <param name="symbol">the name of the stock.</param>
+    /// <param name="now">the time of the rejected request. </param>
     public void Record(string symbol, DateTime now)
     {
         if(symbol==null){return;}
@@ -44,14 +44,14 @@ public class Rejects : Rejections
             this.reject_symbols[symbol] = new Tuple<DateTime, int>(now, 1);
         }
     }
-/// <param name="symbol">the name of the stock.</param>
-/// <param name="now">the time of the request. </param>
-/// <returns>True if: 
-///1.) Is on the IgnoreList. 
-///2.) Number of rejections does not exceed max rejections.
-///3.) Time of the request - the last request exceeds the wait time.
-///False otherwise.
-///</returns>
+    /// <param name="symbol">the name of the stock.</param>
+    /// <param name="now">the time of the request. </param>
+    /// <returns>True if: 
+    ///1.) Is on the IgnoreList. 
+    ///2.) Number of rejections does not exceed max rejections.
+    ///3.) Time of the request - the last request exceeds the wait time.
+    ///False otherwise.
+    ///</returns>
     public bool isPermitted(string symbol, DateTime now)
     {
         if(symbol==null){return false;}
